7cec4e930bb3357b0712026b9a5444c28969432c
gauravgd16 <gauravgd16@gmail.com>
2020-08-25 10:12:01 +0200

    add partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,12 @@ class ArticlesController < ApplicationController
   def edit
     @article= Article.find(params[:id])
   end
-
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was deleted successfully"
+    redirect_to articles_path
+  end
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
